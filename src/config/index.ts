2c1feaf1b159536e4e71d8bbb1a93ed7efcd3253
josephpballantyne <josephpballantyne@gmail.com>
2023-09-20 14:03:28 +0100

    sqlz config

--- src/config/index.ts
+++ src/config/index.ts
@@ -8,7 +8,7 @@ const config = {
   db: {
     database: process.env.POSTGRES_DB,
     host: process.env.POSTGRES_HOST || "localhost",
-    port: 5432,
+    port: Number(process.env.POSTGRES_PORT) || 5432,
     username: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
   },
