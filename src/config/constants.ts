b611f3d004cac9850cbe98904ed3896c9a88ec1b
josephpballantyne <josephpballantyne@gmail.com>
2023-09-21 11:20:41 +0100

    move config file

---
+++ src/config/constants.ts
@@ -0,0 +1,3 @@
+export const AUTH_PROVIDERS = ["auth0"];
+export const PAYMENT_PROVIDERS = ["stripe"];
+export const PAYMENT_TYPES = ["single", "recurring"];
--- src/database/index.ts
+++ src/database/index.ts
@@ -1,5 +1,5 @@
 import { Sequelize } from "sequelize";
-import config from "../config/index.js";
+import config from "../config/config.js";
 import logger from "../utils/logger.js";
 import { initUserModel, initUserAssociations } from "../app/user/user.model.js";
 import {
